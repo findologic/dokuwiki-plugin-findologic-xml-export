5424fa155d8846e79a7670134e7a12babdc9a7d9
TheKeyMaster <d.brader@findologic.com>
2017-06-13 11:58:39 +0200

    Test one

---
+++ tests/DokuwikiXMLExportTest.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use getFilesAndDirectories;
+
+
+
+class DokuwikiXMLExportTest extends TestCase
+{
+    public function testGetFilesAndDirectoriesWork ()
+    {
+        //$getFilesAndDirectories = new getFilesAndDirectories("../../data/pages");
+
+    }
+}
