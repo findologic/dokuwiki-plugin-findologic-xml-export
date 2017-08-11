723534c200274e6490ccbcf43b44d07296a2aef8
TheKeyMaster <d.brader@findologic.com>
2017-08-11 11:47:39 +0200

    Change test name

--- _test/GeneralPluginPhpdokuwiki2findologicTest.php
+++ _test/GeneralPluginPhpdokuwiki2findologicTest.php
@@ -5,7 +5,7 @@
  * @group plugin_phpdokuwiki2findologic
  * @group plugins
  */
-class general_plugin_phpdokuwiki2findologic_test extends DokuWikiTest {
+class GeneralPluginPhpdokuwiki2findologicTest extends \DokuWikiTest {
 
     /**
      * Simple test to make sure the plugin.info.txt is in correct format
