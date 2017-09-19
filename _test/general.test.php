8bf5b962d232ea2eb0b7141c8381f6c62f02a294
TheKeyMaster <d.brader@findologic.com>
2017-09-19 12:44:54 +0200

    Add two test cases.

--- _test/general.test.php
+++ _test/general.test.php
@@ -5,6 +5,9 @@
  * @group plugin_findologicxmlexport
  * @group plugins
  */
+
+require_once (__DIR__ . '/../DokuwikiXMLExport.php');
+require_once (__DIR__ . '/../admin.php');
 class general_plugin_findologicxmlexport_test extends DokuWikiTest {
 
     /**
@@ -57,5 +60,47 @@ class general_plugin_findologicxmlexport_test extends DokuWikiTest {
             }
         }
 
+    }
+
+    /**
+     * Test to ensure that XML is valid when DokuWiki has no pages.
+     */
+    public function test_xml_response_is_valid_if_dokuwiki_has_no_pages() {
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(0, 20));
+
+        $start = implode('', ($xml->xpath('/findologic/items/@start')));
+        $count = implode('', ($xml->xpath('/findologic/items/@count')));
+        $total = implode('', ($xml->xpath('/findologic/items/@total')));
+
+        $expected = 0;
+        $this->assertEquals($expected, $start, 'Expected start value should match "0" when DokuWiki has no pages.');
+        $this->assertEquals($expected, $count, 'Expected count value should match "0" when DokuWiki has no pages.');
+        $this->assertEquals($expected, $total, 'Expected total value should match "0" when DokuWiki has no pages.');
+    }
+
+    /**
+     * Test to ensure that XML is valid when DokuWiki has one page.
+     */
+    public function test_xml_response_is_valid_if_dokuwiki_has_one_page() {
+        saveWikiText('home', '==== Home ==== Some demo text.', 'Some minor changes were made to this page.', true);
+        idx_addPage('home', '', '');
+
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(0, 20));
+
+        $start = implode('', ($xml->xpath('/findologic/items/@start')));
+        $count = implode('', ($xml->xpath('/findologic/items/@count')));
+        $total = implode('', ($xml->xpath('/findologic/items/@total')));
+
+        $expectedStart = 0;
+        $expectedCount = 1;
+        $expectedTotal = 1;
+
+        $this->assertEquals($expectedStart, $start, 'Expected start value should match "0" when DokuWiki has one page.');
+        $this->assertEquals($expectedCount, $count, 'Expected count value should match "1" when DokuWiki has one page.');
+        $this->assertEquals($expectedTotal, $total, 'Expected total value should match "1" when DokuWiki has one page.');
     }
 }
