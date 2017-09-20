17d6e3623aad5c906c78992d5f28316d0fc6f772
TheKeyMaster <d.brader@findologic.com>
2017-09-20 17:39:26 +0200

    Add tests for XML validation on one page

--- _test/general.test.php
+++ _test/general.test.php
@@ -11,6 +11,14 @@ require_once (__DIR__ . '/../admin.php');
 class general_plugin_findologicxmlexport_test extends DokuWikiTest {
 
     /**
+     * Remove all DokuWiki pages before each test
+     */
+    public function setUp(){
+        $indexer = new Doku_Indexer();
+        $indexer->clear();
+    }
+
+    /**
      * Simple test to make sure the plugin.info.txt is in correct format
      */
     public function test_plugininfo() {
@@ -84,7 +92,7 @@ class general_plugin_findologicxmlexport_test extends DokuWikiTest {
      * Test to ensure that XML is valid when DokuWiki has one page.
      */
     public function test_xml_response_is_valid_if_dokuwiki_has_one_page() {
-        saveWikiText('home', '==== Home ==== Some demo text.', 'Some minor changes were made to this page.', true);
+        saveWikiText('home', '==== Home ==== Some demo text.', '');
         idx_addPage('home', '', '');
 
         $conf = array();
@@ -102,5 +110,102 @@ class general_plugin_findologicxmlexport_test extends DokuWikiTest {
         $this->assertEquals($expectedStart, $start, 'Expected start value should match "0" when DokuWiki has one page.');
         $this->assertEquals($expectedCount, $count, 'Expected count value should match "1" when DokuWiki has one page.');
         $this->assertEquals($expectedTotal, $total, 'Expected total value should match "1" when DokuWiki has one page.');
+    }
+
+    /**
+     * Test to ensure that XML is valid when DokuWiki has two pages.
+     */
+    public function test_xml_response_is_valid_if_dokuwiki_has_two_pages() {
+        $pageId1 = 'home1';
+        $pageId2 = 'home2';
+        saveWikiText($pageId1, '==== Home1 ==== Some demo text.', '');
+        saveWikiText($pageId2, '==== Home2 ==== Some demo text.', '');
+
+        idx_addPage($pageId1, '', '');
+        idx_addPage($pageId2, '', '');
+
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(0, 20));
+
+        $start = implode('', ($xml->xpath('/findologic/items/@start')));
+        $count = implode('', ($xml->xpath('/findologic/items/@count')));
+        $total = implode('', ($xml->xpath('/findologic/items/@total')));
+
+        $expectedStart = 0;
+        $expectedCount = 2;
+        $expectedTotal = 2;
+
+        $this->assertEquals($expectedStart, $start, 'Expected start value should match "0" when DokuWiki has two pages.');
+        $this->assertEquals($expectedCount, $count, 'Expected count value should match "2" when DokuWiki has two pages.');
+        $this->assertEquals($expectedTotal, $total, 'Expected total value should match "2" when DokuWiki has two pages.');
+    }
+    public function test_xml_elements_equal_to_dokuwiki_page_data() {
+        $pageId = 'test123:test123:test123';
+        $pageTitle = 'test123';
+        $pageContent = 'This page is for test purposes.';
+        saveWikiText($pageId, $pageContent, '');
+        idx_addPage($pageId, '', '');
+
+        // Set title manually because you cant save it with the saveWikiText function.
+        $pageMetaTitle = array('title' => $pageTitle);
+        p_set_metadata($pageId, $pageMetaTitle);
+
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(0, 20));
+
+        $names = $xml->xpath('/findologic/items/item/names/name');
+        $name = $names[0];
+
+        $summaries = $xml->xpath('/findologic/items/item/summaries/summary');
+        $summary = $summaries[0];
+
+        $descriptions = $xml->xpath('/findologic/items/item/descriptions/description');
+        $description = $descriptions[0];
+
+        $ordernumbers = $xml->xpath('/findologic/items/item/allOrdernumbers/ordernumbers/ordernumber');
+        $ordernumber = $ordernumbers[0];
+
+        $urls = $xml->xpath('/findologic/items/item/urls/url');
+        $url = $urls[0];
+
+        $properties = $xml->xpath('/findologic/items/item/allProperties/properties/property');
+        $propertyKey = (string)$properties[0]->key[0];
+        $propertyValue = (string)$properties[0]->value[0];
+
+        $attributes = $xml->xpath('/findologic/items/item/allAttributes/attributes/attribute');
+        $attributeKey = (string)$attributes[0]->key[0];
+        $attributeValue = (string)$attributes[0]->values[0]->value[0];
+
+        $dateAddeds = $xml->xpath('/findologic/items/item/dateAddeds');
+        $dateAdded = (string)$dateAddeds[0]->dateAdded[0];
+
+        $expectedName = $pageTitle;
+        $expectedSummary = $expectedDescription = $pageContent;
+        $expectedOrdernumber = $pageId;
+        $expectedUrl = 'http://wiki.example.com/./doku.php?id=' . $pageId;
+        $expectedPropertyKey = 'dummy';
+        $expectedPropertyValue = 'dummy';
+        $expectedAttributeKey = 'cat';
+        $expectedAttributeValue = 'test123_test123_test123';
+        // Format DateTime because the creation date is not known. It can vary.
+        $pageMetadata = p_get_metadata($pageId);
+        $pageCreated = new DateTime();
+        $pageCreated->setTimestamp($pageMetadata["date"]["created"]);
+        $expectedDateAdded = (string)$pageCreated->format(\DateTime::ATOM);
+
+        $this->assertEquals($expectedName, $name, 'Expected name in XML should match the pages first title "test123".');
+        $this->assertEquals($expectedSummary, $summary, 'Expected summary in XML should match the pages content "This page is for test purposes.".');
+        $this->assertEquals($expectedDescription, $description, 'Expected description in XML should match the pages content "This page is for test purposes.".');
+        $this->assertEquals($expectedOrdernumber, $ordernumber, 'Expected ordernumber in XML should match the pages namespace "test123:test123:test123".');
+        $this->assertEquals($expectedUrl, $url, 'Expected url in XML should match the DokuWiki-URL and the pages namespace "http://wiki.example.com/./doku.php?id=test123:test123:test123".');
+        $this->assertEquals($expectedPropertyKey, $propertyKey, 'Expected property key in XML should match the dummy value "dummy".');
+        $this->assertEquals($expectedPropertyValue, $propertyValue, 'Expected property value in XML should match the dummy value "dummy".');
+        $this->assertEquals($expectedAttributeKey, $attributeKey, 'Expected attribute key in XML should match the category value "cat".');
+        $this->assertEquals($expectedAttributeValue, $attributeValue, 'Expected attribute value in XML should match the namespace formatted in a FINDOLOGIC proper format "test123_test123_test123".');
+        $this->assertEquals($expectedDateAdded, $dateAdded, 'Expected dateAdded value in XML should match the created date of the page. Value can vary.');
+
+        print_r($DokuwikiXMLExport->generateXMLExport(0, 20));
     }
 }
