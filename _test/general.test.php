4f33049af9f031b3f32b473b0521d6d55d0184f8
TheKeyMaster <d.brader@findologic.com>
2017-09-21 12:30:04 +0200

    Add more test cases

--- _test/general.test.php
+++ _test/general.test.php
@@ -18,6 +18,10 @@ class general_plugin_findologicxmlexport_test extends DokuWikiTest {
         $indexer->clear();
     }
 
+    //======================================================================
+    // GENERAL PLUGIN TESTS
+    //======================================================================
+
     /**
      * Simple test to make sure the plugin.info.txt is in correct format
      */
@@ -69,6 +73,10 @@ class general_plugin_findologicxmlexport_test extends DokuWikiTest {
         }
 
     }
+
+    //======================================================================
+    // XML TESTS
+    //======================================================================
 
     /**
      * Test to ensure that XML is valid when DokuWiki has no pages.
@@ -140,6 +148,10 @@ class general_plugin_findologicxmlexport_test extends DokuWikiTest {
         $this->assertEquals($expectedCount, $count, 'Expected count value should match "2" when DokuWiki has two pages.');
         $this->assertEquals($expectedTotal, $total, 'Expected total value should match "2" when DokuWiki has two pages.');
     }
+
+    /**
+     * Test to ensure that XML outputs basic export data.
+     */
     public function test_xml_elements_equal_to_dokuwiki_page_data() {
         $pageId = 'test123:test123:test123';
         $pageTitle = 'test123';
@@ -205,5 +217,212 @@ class general_plugin_findologicxmlexport_test extends DokuWikiTest {
         $this->assertEquals($expectedAttributeKey, $attributeKey, 'Expected attribute key in XML should match the category value "cat".');
         $this->assertEquals($expectedAttributeValue, $attributeValue, 'Expected attribute value in XML should match the namespace formatted in a FINDOLOGIC proper format "test123_test123_test123".');
         $this->assertEquals($expectedDateAdded, $dateAdded, 'Expected dateAdded value in XML should match the created date of the page. Value can vary.');
+    }
+
+    /**
+     * Test to ensure that if no title is given, the XML will have the namespace well formated as title.
+     */
+    public function test_xml_element_name_is_formatted_namespace_from_dokuwiki_page_data() {
+        $pageId = 'test321:test321:test321';
+        $pageContent = 'This page is for test purposes.';
+        saveWikiText($pageId, $pageContent, '');
+        idx_addPage($pageId, '', '');
+
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(0, 20));
+
+        $names = $xml->xpath('/findologic/items/item/names/name');
+        $name = (string)$names[0];
+
+        $expectedName = 'Test321 Test321 Test321';
+        $this->assertEquals($expectedName, $name, 'Expected name in XML should be the namespace of the page formatted "Test321 Test321 Test321".');
+    }
+
+    /**
+     * Test to ensure that ordernumber and attribute categories in the XML are valid when DokuWiki has a namespace height of two.
+     */
+    public function test_xml_elements_equal_to_dokuwiki_page_data_when_namespace_height_is_two() {
+        $pageId = 'test123:test123';
+        $pageContent = 'This page is for test purposes.';
+        saveWikiText($pageId, $pageContent, '');
+        idx_addPage($pageId, '', '');
+
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(0, 20));
+
+        $ordernumbers = $xml->xpath('/findologic/items/item/allOrdernumbers/ordernumbers/ordernumber');
+        $ordernumber = $ordernumbers[0];
+
+        $attributes = $xml->xpath('/findologic/items/item/allAttributes/attributes/attribute');
+        $attributeKey = (string)$attributes[0]->key[0];
+        $attributeValue = (string)$attributes[0]->values[0]->value[0];
+
+        $expectedOrdernumber = $pageId;
+        $expectedAttributeKey = 'cat';
+        $expectedAttributeValue = 'test123_test123';
+
+        $this->assertEquals($expectedAttributeKey, $attributeKey, 'Expected attribute key in XML should match the category value when namespace height is two "cat".');
+        $this->assertEquals($expectedAttributeValue, $attributeValue, 'Expected attribute value in XML should match the namespace formatted in a FINDOLOGIC proper format when namespace height is two "test123_test123".');
+        $this->assertEquals($expectedOrdernumber, $ordernumber, 'Expected ordernumber in XML should match the pages namespace when namespace height is two "test123:test123".');
+    }
+
+    /**
+     * Test to ensure that ordernumber and attribute categories in the XML are valid when DokuWiki has a namespace height of one.
+     */
+    public function test_xml_elements_equal_to_dokuwiki_page_data_when_namespace_height_is_one() {
+        $pageId = 'test321';
+        $pageContent = 'This page is for test purposes.';
+        saveWikiText($pageId, $pageContent, '');
+        idx_addPage($pageId, '', '');
+
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(0, 20));
+
+        $ordernumbers = $xml->xpath('/findologic/items/item/allOrdernumbers/ordernumbers/ordernumber');
+        $ordernumber = $ordernumbers[0];
+
+        $attributes = $xml->xpath('/findologic/items/item/allAttributes/attributes/attribute');
+        $attributeKey = (string)$attributes[0]->key[0];
+        $attributeValue = (string)$attributes[0]->values[0]->value[0];
+
+        $expectedOrdernumber = $pageId;
+        $expectedAttributeKey = 'cat';
+        $expectedAttributeValue = 'test321';
+
+        $this->assertEquals($expectedAttributeKey, $attributeKey, 'Expected attribute key in XML should match the category value when namespace height is two "cat".');
+        $this->assertEquals($expectedAttributeValue, $attributeValue, 'Expected attribute value in XML should match the namespace formatted in a FINDOLOGIC proper format when namespace height is two "test123_test123".');
+        $this->assertEquals($expectedOrdernumber, $ordernumber, 'Expected ordernumber in XML should match the pages namespace when namespace height is two "test123:test123".');
+    }
+
+    //======================================================================
+    // EXPORT CALL TESTS
+    //======================================================================
+
+    /**
+     * Test to ensure that parameters are correctly handled in the export call.
+     * start = 1, count = 1, total = 1;
+     * @expectedException InvalidArgumentException
+     */
+    public function test_call_export_with_start_value_one() {
+        $pageId = 'demopage1';
+        $pageContent = 'This page is for test purposes.';
+        saveWikiText($pageId, $pageContent, '');
+        idx_addPage($pageId, '', '');
+
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(1, 1));
+    }
+
+    /**
+     * Test to ensure that parameters are correctly handled in the export call.
+     * start = 2, count = 1, total = 1;
+     * @expectedException InvalidArgumentException
+     */
+    public function test_call_export_with_start_value_two() {
+        $pageId = 'demopage2';
+        $pageContent = 'This page is for test purposes.';
+        saveWikiText($pageId, $pageContent, '');
+        idx_addPage($pageId, '', '');
+
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(2, 1));
+    }
+
+    /**
+     * Test to ensure that parameters are correctly handled in the export call.
+     * start = 0, count = 0, total = 1;
+     * @expectedException InvalidArgumentException
+     */
+    public function test_call_export_with_count_value_zero() {
+        $pageId = 'demopage3';
+        $pageContent = 'This page is for test purposes.';
+        saveWikiText($pageId, $pageContent, '');
+        idx_addPage($pageId, '', '');
+
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(0, 0));
+    }
+
+    /**
+     * Test to ensure that parameters are correctly handled in the export call.
+     * start = 1, count = 0, total = 1;
+     * @expectedException InvalidArgumentException
+     */
+    public function test_call_export_with_count_value_zero_and_start_one() {
+        $pageId = 'demopage4';
+        $pageContent = 'This page is for test purposes.';
+        saveWikiText($pageId, $pageContent, '');
+        idx_addPage($pageId, '', '');
+
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(1, 0));
+    }
+
+    /**
+     * Test to ensure that parameters are correctly handled in the export call.
+     * start = 0, count = -1, total = 1;
+     * @expectedException InvalidArgumentException
+     */
+    public function test_call_export_with_count_value_below_zero() {
+        $pageId = 'demopage5';
+        $pageContent = 'This page is for test purposes.';
+        saveWikiText($pageId, $pageContent, '');
+        idx_addPage($pageId, '', '');
+
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(0, -1));
+    }
+
+    /**
+     * Test to ensure that parameters are correctly handled in the export call.
+     * start = -1, count = 1, total = 1;
+     * @expectedException InvalidArgumentException
+     */
+    public function test_call_export_with_start_value_below_zero() {
+        $pageId = 'demopage6';
+        $pageContent = 'This page is for test purposes.';
+        saveWikiText($pageId, $pageContent, '');
+        idx_addPage($pageId, '', '');
+
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(-1, 0));
+    }
+
+    /**
+     * Test to ensure that parameters are correctly handled in the export call.
+     * start = 0, count = 10, total = 9;
+     */
+    public function test_call_export_with_count_greater_than_total() {
+        $pageId = array();
+        $pageContent = 'This page is for test purposes.';
+        for ($i=1; $i<=9; $i++){
+            $pageId[$i] = 'demopage0' . $i;
+            saveWikiText($pageId[$i], $pageContent, '');
+            idx_addPage($pageId[$i], '', '');
+        }
+
+        $conf = array();
+        $DokuwikiXMLExport = new DokuwikiXMLExport($conf);
+        $xml = new SimpleXMLElement($DokuwikiXMLExport->generateXMLExport(0, 10));
+
+        $start = implode('', ($xml->xpath('/findologic/items/@start')));
+        $count = implode('', ($xml->xpath('/findologic/items/@count')));
+        $total = implode('', ($xml->xpath('/findologic/items/@total')));
+
+        $expectedStart = 0;
+        $expectedCount = $expectedTotal = 9;
+
+        $this->assertEquals($expectedStart, $start, 'Expected start value should match "0" when export is called with start "0".');
+        $this->assertEquals($expectedCount, $count, 'Expected count value should match "9" when export is called with "10" and $total is "9".');
+        $this->assertEquals($expectedTotal, $total, 'Expected total value should match "9" when DokuWiki has 9 pages.');
     }
 }
